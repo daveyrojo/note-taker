d2e7b5370d43286633a211c3044ba7ef9f7893fb
David Eldridge <d.vincenteldridge@gmail.com>
2021-03-08 23:30:01 -0500

    working on delete function

--- db/store.js
+++ db/store.js
@@ -1,20 +1,8 @@
 //readNotes ... 
 const fs = require('fs');
-const util = require('util');
-const writeFile = util.promisify(fs.writeFile);
-const readFile = util.promisify(fs.readFile);
+
 
 const storedNotes = [{
-        getNotes: async() => {
-            try {
-                const notes = await readFile(`db.json`);
-                return JSON.parse(notes);
-            } catch(err) {
-                console.error(err);
-            }
-        }
-    },
-    {
         writeNotes: async() => {
             try {
                 const newNote = await writeFileAsync(`db.jason`, JSON.stringify(data));
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,39 +1,42 @@
 // import items needed
 const router = require('express').Router();
-const store = require('../db/store')
+// const store = require('../db/store')
 const noteData = require('../db/db.json')
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 // make a GET request with all notes from the database
 
 router.get('/notes', (req, res) => {
+    console.log(noteData);
     res.json(noteData);
 })
 
 router.post('/notes', (req, res) => {
-    req.body.id = Math.floor(Math.random() * Math.floor(1000));
+    req.body.id = uuidv4();
+    console.log(req.body.id);
     noteData.push(req.body);
     fs.writeFileSync('./db/db.json', JSON.stringify(noteData));
 })
 
-router.post('/notes', (req, res) => {
+router.delete('/notes/:id', (req, res) => {
+    console.log(req.params.id);
+    fs.readFile('./db/db.json', (err, data) => {
+        if (err) throw err;
+        let noteInfo = JSON.parse(data);
+        console.log(noteInfo);
+        for (let i = 0; i < noteInfo.length; i++) {
+            let note = noteInfo[i];
+            if (note.id == req.params.id) {
+                console.log('this is the note you are looking for');
+                noteInfo.slice(i, 1);
+                console.log(noteInfo);
+            }
+        }
+        fs.writeFileSync('./db/db.json', JSON.stringify(noteInfo));
+    })
 
+    res.json(noteData)
 })
-// router.get('/notes', (req, res) => {
-//     store
-//     .getNotes()
-//     .then((notes) => {
-//         return res.json(notes);
-//     })
-//     .catch((err) => res.status(500).json(err));
-// })
-
-// // create a post request
-// router.post('/api/notes', async (req, res) => {
-//     const newNote = await store.getNotes();
-//     res.json(newNote);
-// });
-
-// // create a delete request
 
 
 module.exports = router;
